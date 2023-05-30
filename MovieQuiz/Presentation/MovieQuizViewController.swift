efb56d541b7e5c25d9a8964f671e291b15a4c17e
vano3d <vano3d@gmail.com>
2023-05-31 00:14:35 +0300

    Исправления по замечаниям в ревью: - поправлена очерёдность элементов внутри класса - убрал сильный захват в UIAlertAction - убрал лишние пустые строки - поменял guard на if-else - завернул в функцию  yesButton.isEnabled - поменял Aspect Fit на Aspect Fill - исправил баг с заблокированными кнопками

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -55,16 +55,28 @@ private let questions: [QuizQuestion] = [
             correctAnswer: false)
     ]
 
-
 final class MovieQuizViewController: UIViewController {
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
     
     @IBOutlet private var yesButton: UIButton!
     @IBOutlet private var noButton: UIButton!
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var textLabel: UILabel!
-    private var currentQuestionIndex = 0
-    private var correctAnswers = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        show(quiz: convert(model: questions[0]))
+        noButton.layer.cornerRadius = 15
+        yesButton.layer.cornerRadius = 15
+        imageView.layer.cornerRadius = 20
+    }
+    private func isEnabledButton(_ isEnabled: Bool) {
+        yesButton.isEnabled = isEnabled
+        noButton.isEnabled = isEnabled
+    }
     
     private func showAlert(alertTitle: String, alertMessage: String, alertButtonTitle: String) {
         let alert = UIAlertController(
@@ -72,26 +84,18 @@ final class MovieQuizViewController: UIViewController {
             message: alertMessage,
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: alertButtonTitle, style: .default) { [self] _ in
-            currentQuestionIndex = 0
-            let firstQuestion = questions[currentQuestionIndex]
-            let viewModel = convert(model: firstQuestion)
-            show(quiz: viewModel)
-            correctAnswers = 0
-            imageView.layer.borderWidth = 0
-            
+        let action = UIAlertAction(title: alertButtonTitle, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            let firstQuestion = questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+            self.correctAnswers = 0
+            self.imageView.layer.borderWidth = 0
+            self.isEnabledButton(true)
         }
+        
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        show(quiz: convert(model: questions[0]))
-        noButton.layer.cornerRadius = 15
-        yesButton.layer.cornerRadius = 15
-        imageView.layer.cornerRadius = 20
-        
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
@@ -102,63 +106,55 @@ final class MovieQuizViewController: UIViewController {
         return questionStep
     }
     
-    
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
     
-    
     private func showNextQuestionOrResults() {
-        guard currentQuestionIndex == questions.count - 1 else {
+        if currentQuestionIndex == questions.count - 1 {
+            showAlert(
+                alertTitle: "Этот раунд окончен!",
+                alertMessage: "Ваш результат: \(correctAnswers)/\(questions.count)",
+                alertButtonTitle: "Сыграть ещё раз")
+        } else {
             currentQuestionIndex += 1
             let nextQuestion = questions[currentQuestionIndex]
             let viewModel = convert(model: nextQuestion)
             show(quiz: viewModel)
             imageView.layer.borderWidth = 0
-            yesButton.isEnabled = true
-            noButton.isEnabled = true
-            return
+            isEnabledButton(true)
         }
-            showAlert(
-                alertTitle: "Этот раунд окончен!",
-                alertMessage: "Ваш результат: \(correctAnswers)/\(questions.count)",
-                alertButtonTitle: "Сыграть ещё раз")
     }
-    
     
     private func showAnswerResult(isCorrect: Bool) {
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
-        yesButton.isEnabled = false
-        noButton.isEnabled = false
+        isEnabledButton(false)
         
-        guard isCorrect else {
+        if isCorrect {
+            imageView.layer.borderColor = UIColor.ypGreen.cgColor
+            correctAnswers += 1
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.showNextQuestionOrResults()
+            }
+        } else {
             imageView.layer.borderColor = UIColor.ypRed.cgColor
             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 self.showNextQuestionOrResults()
             }
-            return
-        }
-        imageView.layer.borderColor = UIColor.ypGreen.cgColor
-        correctAnswers += 1
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.showNextQuestionOrResults()
         }
     }
     
         @IBAction private func yesButtonClicked(_ sender: UIButton) {
             let currentQuestion = questions[currentQuestionIndex]
             showAnswerResult(isCorrect: currentQuestion.correctAnswer == true)
-   
         }
         
         @IBAction private func noButtonClicked(_ sender: UIButton) {
             let currentQuestion = questions[currentQuestionIndex]
             showAnswerResult(isCorrect: currentQuestion.correctAnswer == false)
-
-            
         }
     }
     
