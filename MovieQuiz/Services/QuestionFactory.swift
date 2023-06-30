0de2cdec37bf4b4840293ae15121c6b6d95698c2
vano3d <vano3d@gmail.com>
2023-07-01 00:29:55 +0300

    добавил рандомный рейтинг в вопросе

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -27,9 +27,11 @@ class QuestionFactory: QuestionFactoryProtocol {
             }
             
             let rating = Float(movie.rating) ?? 0
+            print(rating)
+            let randomRating = Float.random(in: 7.5...9.5)
             
-            let text = "Рейтинг этого фильма больше чем 7?"
-            let correctAnswer = rating > 7
+            let text = "Рейтинг этого фильма больше чем \(randomRating)?"
+            let correctAnswer = rating > randomRating
             
             let question = QuizQuestion(image: imageData,
                                          text: text,
